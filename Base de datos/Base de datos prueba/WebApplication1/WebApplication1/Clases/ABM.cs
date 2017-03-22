9a60f51d22376304fe12fd214e9ad091cfd5a904
arielrando <ariel_rando@hotmail.com.ar>
2017-03-22 11:34:39 -0300

    Base de datos prueba (1)
    se agrega un proyecto ejemplo para una agregar un solo registro a una
    base de datos

---
+++ Base de datos/Base de datos prueba/WebApplication1/WebApplication1/Clases/ABM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Clases
+{
+    public class ABM
+    {
+        public void Subir1Pelicula()
+        {
+            using (BonaFilmsEntities context = new BonaFilmsEntities())
+            {
+                Peliculas pelicula = new Peliculas
+                {
+                    Nombre = "Duro de matar",
+                    Año = 2001,
+                    Cantidad = 10,
+                    Disponibilidad = true
+                };
+
+                context.Peliculas.Add(pelicula);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Base de datos/Base de datos prueba/WebApplication1/WebApplication1/Web_Forms/PaginaEjemplo.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1.Web_Forms
+{
+    public partial class PaginaEjemplo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Clases.ABM p = new Clases.ABM();
+
+            p.Subir1Pelicula();
+        }
+    }
+}
